e38e5e7db18281b1abf8c0531a0d42d03d1f595c
Tropicaljittai <93302274+Tropicaljittai@users.noreply.github.com>
2022-12-19 15:06:23 +0700

    Update main.c

--- 04-array-comparison/main.c
+++ 04-array-comparison/main.c
@@ -2,19 +2,20 @@
 
 int main()
 {
-    int m, c=0;
-    m = 1;
+    int m, c, n;
+    c = 0;
+    m = 5;
     int i[] = {1, 2 ,3};
     int q[] = {5, 1, 1};
-    for (int k = 0; k < m; k++){
-            if (m==i[k]){
-                c += 1;
-            }
-            else if (m==q[k]){
-                c += 1;
-            }
+    for (int k = 0; k < sizeof(i)/4; k++){
+        if (m == i[k]){
+            c += 1;
+        }
+        else if (m == q[k]){
+            c += 1;
+        }
     }
-    if (c=2 || c>2){
+    if (c>=2){
         printf("0");
     }
     else{
